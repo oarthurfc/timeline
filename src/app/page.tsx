0141497a1ec2ce466d6c3af8b4295adbf88bec14
oarthurfc <arthuryouface@gmail.com>
2025-09-15 19:34:48 -0300

    feat: add initial timeline layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+import { Calendar, GripVertical } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="mx-auto flex w-full max-w-7xl flex-col items-center gap-[32px] py-14">
+      <h1 className="text-center text-3xl font-bold">Horizontal Timeline</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Start Timeline Container */}
+      <div className="w-full overflow-x-auto">
+        {/* Start Timeline Header */}
+        <div className="flex justify-between py-4 text-sm text-gray-500">
+          <p className="font-bold">YYYY/MM/DD</p>
+          <p>MM/DD</p>
+          <p>MM/DD</p>
+          <p>MM/DD</p>
+          <p className="font-bold">YYYY/MM/DD</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {/* End Timeline Header */}
+
+        {/* Start Timeline Lanes */}
+        <div className="flex gap-6 border-y border-gray-200 px-2 py-6">
+          {/* Start Timeline Events */}
+          <div className="bg-background shadow-event flex items-center gap-4 rounded-md border-l-4 border-l-green-500 py-3 pr-7 pl-4">
+            <GripVertical className="cursor-grab text-gray-600" />
+            <div className="flex flex-col gap-0.5 text-sm font-bold">
+              <p className="flex gap-2">Plann Sprint Duration</p>
+              <div className="flex items-center gap-1.5 text-indigo-400">
+                <Calendar className="w-4" strokeWidth={2} />
+                <p>Jan 31 - Feb 4</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-background shadow-event flex items-center gap-4 rounded-md border-l-4 border-l-green-500 py-3 pr-7 pl-4">
+            <GripVertical className="cursor-grab text-gray-600" />
+            <div className="flex flex-col gap-0.5 text-sm font-bold">
+              <p className="flex gap-2">Plann Sprint Duration</p>
+              <div className="flex items-center gap-1.5 text-indigo-400">
+                <Calendar className="w-4" strokeWidth={2} />
+                <p>Jan 31 - Feb 4</p>
+              </div>
+            </div>
+          </div>
+          {/* End Timeline Events */}
+        </div>
+        <div className="flex gap-6 border-y border-gray-200 px-2 py-6">
+          {/* Start Timeline Events */}
+          <div className="bg-background shadow-event flex items-center gap-4 rounded-md border-l-4 border-l-green-500 py-3 pr-7 pl-4">
+            <GripVertical className="cursor-grab text-gray-600" />
+            <div className="flex flex-col gap-0.5 text-sm font-bold">
+              <p className="flex gap-2">Plann Sprint Duration</p>
+              <div className="flex items-center gap-1.5 text-indigo-400">
+                <Calendar className="w-4" strokeWidth={2} />
+                <p>Jan 31 - Feb 4</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-background shadow-event flex items-center gap-4 rounded-md border-l-4 border-l-green-500 py-3 pr-7 pl-4">
+            <GripVertical className="cursor-grab text-gray-600" />
+            <div className="flex flex-col gap-0.5 text-sm font-bold">
+              <p className="flex gap-2">Plann Sprint Duration</p>
+              <div className="flex items-center gap-1.5 text-indigo-400">
+                <Calendar className="w-4" strokeWidth={2} />
+                <p>Jan 31 - Feb 4</p>
+              </div>
+            </div>
+          </div>
+          {/* End Timeline Events */}
+        </div>
+
+        {/* End Timeline Lanes */}
+      </div>
+      {/* End Timeline Container */}
+    </main>
   );
 }
